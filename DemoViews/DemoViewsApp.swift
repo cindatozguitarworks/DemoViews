a2c75ed4f966f1f43052feea18f6dbbfe6c38435
Cynthia Anderson <cind@ozguitarworks.com>
2021-04-06 13:18:38 -0700

    Initial Commit

---
+++ DemoViews/DemoViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoViewsApp.swift
+//  DemoViews
+//
+//  Created by Cynthia Anderson on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
